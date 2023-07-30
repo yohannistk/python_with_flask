748a528d15143f90fe1460a74ed4b20e704618e1
john <johngod2116@gmail.com>
2023-07-30 14:36:45 +0300

    requirements.txt added

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 
-from flask import Flask, request
-
+from flask import Flask, request, jsonify
+from diffusers import DiffusionPipeline
+import torch
 app = Flask(__name__)
 
 @app.route('/process_string', methods=['GET'])
@@ -9,8 +10,15 @@ def process_string():
     
     if string_param:
         # Process the string here
-        processed_string = string_param.upper()  # Example: convert to uppercase
-        return f"Processed string: {processed_string}"
+        pipe = DiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-xl-base-1.0", torch_dtype=torch.float16, use_safetensors=True, variant="fp16")
+        # pipe.to("cuda")
+
+        # if using torch < 2.0
+        # pipe.enable_xformers_memory_efficient_attention()
+
+        images = pipe(prompt=string_param)
+        # processed_string = string_param.upper()  # Example: convert to uppercase
+        return f"Processed string: {jsonify(images)}"
     else:
         return "No string parameter provided."
 
