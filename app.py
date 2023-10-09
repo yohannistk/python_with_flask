8eca9dc8e28d1e7a5dcaab4139bcb214f357a265
john <johngod2116@gmail.com>
2023-10-09 16:58:03 +0300

    First Commot

--- app.py
+++ app.py
@@ -1,26 +1,14 @@
-
+from transformers import pipeline
 from flask import Flask, request, jsonify
-from diffusers import DiffusionPipeline
-import torch
+
 app = Flask(__name__)
 
-@app.route('/process_string', methods=['GET'])
-def process_string():
-    string_param = request.args.get('string')
-    
-    if string_param:
-        # Process the string here
-        pipe = DiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-xl-base-1.0", torch_dtype=torch.float16, use_safetensors=True, variant="fp16")
-        # pipe.to("cuda")
+pipe = pipeline("text-classification", model="Hello-SimpleAI/chatgpt-detector-roberta")
 
-        # if using torch < 2.0
-        # pipe.enable_xformers_memory_efficient_attention()
-
-        images = pipe(prompt=string_param)
-        # processed_string = string_param.upper()  # Example: convert to uppercase
-        return f"Processed string: {jsonify(images)}"
-    else:
-        return "No string parameter provided!!!."
+@app.route('/api/detect-gpt-content', methods=['POST'])
+def process_data():
+    prompt = request.get_json()['prompt']  # Get JSON data from the request
+    return jsonify(pipe(prompt))
 
 if __name__ == '__main__':
     app.run()
